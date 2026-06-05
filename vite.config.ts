50607ca0ec435d4e972ace7a8688e73cc1d3d088
Nandan Patel <nandan.jp17@gmail.com>
2026-06-05 12:56:06 -0700

    best practices optimizations

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
         port: 3000
     },
     build: {
+        sourcemap: true,
         rollupOptions: {
             output: {
                 manualChunks(id) {
